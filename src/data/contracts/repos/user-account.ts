7c5152cd8057da38543db0a37c847fb2a4796783
Fernando Tomio <nandotomio@gmail.com>
2022-01-13 18:17:27 -0300

    ensure Service updates account if LoadUserAccountRepo returns data

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -6,7 +6,10 @@ export namespace LoadUserAccountRepository {
   export type Params = {
     email: string
   }
-  export type Result = undefined
+  export type Result = undefined | {
+    id: string
+    name?: string
+  }
 }
 
 export interface CreateFacebookAccountRepository {
@@ -17,6 +20,18 @@ export namespace CreateFacebookAccountRepository {
   export type Params = {
     name: string
     email: string
+    facebookId: string
+  }
+}
+
+export interface UpdateFacebookAccountRepository {
+  updateWithFacebook: (params: UpdateFacebookAccountRepository.Params) => Promise<void>
+}
+
+export namespace UpdateFacebookAccountRepository {
+  export type Params = {
+    id: string
+    name: string
     facebookId: string
   }
 }
