a610d7d5576f1b6bde2ab3dc08997292f9a937c2
Fernando Tomio <nandotomio@gmail.com>
2022-01-19 18:38:09 -0300

    ensure PgUserAccountRepository updates account if id is defined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -48,5 +48,26 @@ describe('PgUserAccountRepository', () => {
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
       expect(pgUser?.id).toBe(1)
     })
+
+    it('should update an account if id is defined', async () => {
+      await pgUserRepo.save({
+        name: 'any_name',
+        email: 'any_email',
+        facebookId: 'any_fb_id'
+      })
+      await sut.saveWithFacebook({
+        id: '1',
+        name: 'new_name',
+        email: 'new_email',
+        facebookId: 'new_fb_id'
+      })
+      const pgUser = await pgUserRepo.findOne({ id: 1 })
+      expect(pgUser).toEqual({
+        id: 1,
+        name: 'new_name',
+        email: 'any_email',
+        facebookId: 'new_fb_id'
+      })
+    })
   })
 })
