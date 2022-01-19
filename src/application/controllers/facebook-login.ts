e1b436bbe4f3045b3ede233e6c4eef705c9e4970
Fernando Tomio <nandotomio@gmail.com>
2022-01-19 19:55:21 -0300

    apply ok helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,6 +1,6 @@
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { HttpResponse, badRequest, unauthorized, serverError } from '@/application/helpers'
+import { HttpResponse, badRequest, unauthorized, serverError, ok } from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
 
 export class FacebookLoginController {
@@ -13,12 +13,9 @@ export class FacebookLoginController {
       }
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: accessToken.value
-          }
-        }
+        return ok({
+          accessToken: accessToken.value
+        })
       } else {
         return unauthorized()
       }
