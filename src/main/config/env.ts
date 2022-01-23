74eb337306bd518317bf91624457d77c549031df
Fernando Tomio <nandotomio@gmail.com>
2022-01-23 18:09:23 -0300

    typeorm config to env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,21 @@
+import { ConnectionOptions } from 'typeorm'
+
+export const pgConnection: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST ?? '',
+  port: 5432,
+  username: '',
+  password: '',
+  database: '',
+  entities: ['dist/infra/postgres/entities/index.ts']
+}
+
 export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? 'fb_client_id',
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'fb_client_secret'
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? '923yr312ry39'
+  jwtSecret: process.env.JWT_SECRET ?? '923yr312ry39',
+  pgConnection
 }
