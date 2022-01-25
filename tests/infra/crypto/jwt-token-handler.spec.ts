162afbf1acf73aa5705fb86f4d8012b4d64aa883
Fernando Tomio <nandotomio@gmail.com>
2022-01-25 18:47:02 -0300

    ensure JwtTokenHandler throws on invalid cases

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -68,5 +68,17 @@ describe('JwtTokenHandler', () => {
       const generatedKey = await sut.validateToken({ token })
       expect(generatedKey).toBe(key)
     })
+
+    it('should rethrow if verify throws', async () => {
+      fakeJwt.verify.mockImplementationOnce(() => { throw new Error('key_error') })
+      const promise = sut.validateToken({ token })
+      await expect(promise).rejects.toThrow(new Error('key_error'))
+    })
+
+    it('should throw if verify returns null', async () => {
+      fakeJwt.verify.mockImplementationOnce(() => null)
+      const promise = sut.validateToken({ token })
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
