93abe492d98b11674ba15d191e8f320c5dd121f6
Fernando Tomio <nandotomio@gmail.com>
2022-02-12 16:44:27 -0300

    ensure SavePictureController extends Controller

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError, InvalidMimeTypeError, MaxFileSizeError } from '@/application/errors'
-import { SavePictureController } from '@/application/controllers'
+import { Controller, SavePictureController } from '@/application/controllers'
 
 describe('SavePictureController', () => {
   let buffer: Buffer
@@ -19,6 +19,10 @@ describe('SavePictureController', () => {
 
   beforeEach(() => {
     sut = new SavePictureController(changeProfilePicture)
+  })
+
+  it('should extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
   })
 
   it('should return 400 if file is not provided', async () => {
