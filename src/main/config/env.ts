31ba53f40c0c28d64960fae0212e4343e1051fc3
Fernando Tomio <nandotomio@gmail.com>
2022-02-21 21:50:41 -0300

    add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -13,7 +13,14 @@ export const pgConnection: ConnectionOptions = {
 export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? 'fb_client_id',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? 'fb_client_secret'
+    clientSecret: process.env.FB_CLIENT_SECRET ?? 'fb_client_secret',
+    accessToken: process.env.FB_ACCESS_TOKEN ?? 'fb_access_token'
+  },
+  s3: {
+    accessKey: process.env.AWS_ACCESS_KEY_ID ?? '',
+    secret: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    bucket: process.env.AWS_BUCKET ?? '',
+    key: process.env.AWS_KEY ?? ''
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? '923yr312ry39',
---
+++ tests/external/aws-s3-file-storage.test.ts
@@ -0,0 +1,26 @@
+import { AwsS3FileStorage } from '@/infra/gateways'
+import { env } from '@/main/config/env'
+
+import axios from 'axios'
+
+describe('AWS S3 Integration Tests', () => {
+  let sut: AwsS3FileStorage
+
+  beforeEach(() => {
+    sut = new AwsS3FileStorage(env.s3.accessKey, env.s3.secret, env.s3.bucket)
+  })
+
+  it('should upload and delete image to aws s3', async () => {
+    const onePixelImage = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+M/A8B8ABQAB/6Zcm10AAAAASUVORK5CYII='
+    const file = Buffer.from(onePixelImage, 'base64')
+    const fileName = 'any_file_name.png'
+
+    const pictureUrl = await sut.upload({ key: fileName, file })
+
+    expect((await axios.get(pictureUrl)).status).toBe(200)
+
+    await sut.delete({ key: fileName })
+
+    await expect(axios.get(pictureUrl)).rejects.toThrow()
+  })
+})
