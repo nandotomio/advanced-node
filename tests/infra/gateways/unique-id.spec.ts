e3e8d8ef9ebee9c7d03f52e98f274796fec4738b
Fernando Tomio <nandotomio@gmail.com>
2022-03-13 22:39:15 -0300

    change UniqueId to get new date every call

--- tests/infra/gateways/unique-id.spec.ts
+++ tests/infra/gateways/unique-id.spec.ts
@@ -1,15 +1,21 @@
 import { UniqueId } from '@/infra/gateways'
 
+import { set, reset } from 'mockdate'
+
 describe('UniqueId', () => {
-  it('should call uuid.v4', () => {
-    const sut = new UniqueId(new Date(2021, 9, 3, 10, 10, 10))
-    const uuid = sut.uuid({ key: 'any_key' })
-    expect(uuid).toBe('any_key_20211003101010')
+  let sut: UniqueId
+
+  beforeAll(() => {
+    set(new Date(2021, 9, 3, 10, 10, 10))
+    sut = new UniqueId()
   })
 
-  it('should call uuid.v4', () => {
-    const sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0))
+  afterAll(() => {
+    reset()
+  })
+
+  it('should create a unique id', () => {
     const uuid = sut.uuid({ key: 'any_key' })
-    expect(uuid).toBe('any_key_20180310180100')
+    expect(uuid).toBe('any_key_20211003101010')
   })
 })
